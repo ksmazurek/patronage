4657566aabe27ed4486f51f139896e180c6221e5
Kinga Mazurek <parpotkm@gmail.com>
2016-12-21 17:29:26 +0100

    finalna wersja-2

--- script1.js
+++ script1.js
@@ -6,10 +6,6 @@ var el1 = document.querySelector("#myBox");
 function Counting(element){
 	count += 1;
 	element.childNodes[0].nodeValue = count;
-	if (element.childNodes[0].nodeValue == 0){
-		count=0;
-		element.childNodes[0].nodeValue = count;
-	}
 }
 el1.addEventListener("click", function(e){
 	e.stopPropagation();
